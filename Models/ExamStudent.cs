a2de809aa2610a50dc61e3d4f41ba2bca4b1f4f8
AlikM11 <tamerlaailn7@gmail.com>
2022-08-14 16:49:04 +0400

    database

---
+++ Models/ExamStudent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stamford.Models
+{
+    public partial class ExamStudent
+    {
+        public int Id { get; set; }
+        public int Studentid { get; set; }
+        public int Examid { get; set; }
+        public byte Mark { get; set; }
+
+        public virtual Exam Exam { get; set; } = null!;
+        public virtual Student Student { get; set; } = null!;
+    }
+}
