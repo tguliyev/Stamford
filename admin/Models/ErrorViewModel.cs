5916f4f1085b94d8cf11eb61afd35f5bc3cccde3
tguliyev <tguliev9@gmail.com>
2022-08-15 23:33:15 +0400

    add new projs

---
+++ admin/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace admin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ student/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace student.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
